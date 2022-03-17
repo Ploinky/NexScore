d19ddc77062805e09763800c70362d96b9601f27
Johannes Litger <mail@johannes-litger.de>
2022-03-17 13:40:45 +0100

    Initial commit of Spring project

---
+++ src/main/java/de/ploinky/NexScoreApp/NexScoreAppApplication.java
@@ -0,0 +1,13 @@
+package de.ploinky.NexScoreApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexScoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexScoreAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ploinky/NexScoreApp/NexScoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ploinky.NexScoreApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexScoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
