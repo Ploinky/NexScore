74aa78e941a853c4899ea0429e9a48952cf11b2e
Johannes Litger <mail@johannes-litger.de>
2022-03-21 10:52:54 +0100

    Added test for ping endpoint

---
+++ src/test/java/de/ploinky/NexScoreApp/PingTest.java
@@ -0,0 +1,33 @@
+package de.ploinky.NexScoreApp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PingTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testGreetingDefaultMessage() throws Exception {
+        Ping response = new Ping("Hello, World!");
+        final String expectedResponseContent = objectMapper.writeValueAsString(response);
+
+        this.mockMvc.perform(get("/ping")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseContent));
+    }
+}
