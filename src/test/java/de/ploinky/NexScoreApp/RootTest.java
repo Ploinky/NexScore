208b20298b0e52b1bf46f831655217e2e281ee8c
Johannes Litger <mail@johannes-litger.de>
2022-03-21 14:22:55 +0100

    Added root endpoint

---
+++ src/test/java/de/ploinky/NexScoreApp/RootTest.java
@@ -0,0 +1,32 @@
+package de.ploinky.NexScoreApp;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RootTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testRoot() throws Exception {
+        APIDescription response = new APIDescription();
+        final String expectedResponseContent = objectMapper.writeValueAsString(response);
+
+        mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseContent));
+    }
+}
