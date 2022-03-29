e4770b99f6dbbf69afb12230ccfc18abddef7e86
Johannes Litger <mail@johannes-litger.de>
2022-03-29 16:01:01 +0200

    Fixed warnings

--- src/main/java/de/ploinky/NexScoreApp/controller/PlayerController.java
+++ src/main/java/de/ploinky/NexScoreApp/controller/PlayerController.java
@@ -1,11 +1,10 @@
 package de.ploinky.NexScoreApp.controller;
 
-import de.ploinky.NexScoreApp.model.APIDescription;
-import de.ploinky.NexScoreApp.model.Player;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.ploinky.NexScoreApp.model.Player;
 
 @RestController
 public class PlayerController {
--- src/main/java/de/ploinky/NexScoreApp/db/DynamoDBConfig.java
+++ src/main/java/de/ploinky/NexScoreApp/db/DynamoDBConfig.java
@@ -4,15 +4,12 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Region;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import static com.amazonaws.regions.Regions.EU_CENTRAL_1;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "de.ploinky.NexScoreApp.db.repositories")
