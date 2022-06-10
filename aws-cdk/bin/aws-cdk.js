ba1f30215e549362c5cb32f676d77fbe17298bb5
Johannes Litger <mail@johannes-litger.de>
2022-06-10 16:55:22 +0200

    Updated aws cdk to reflect aws changes

--- aws-cdk/bin/aws-cdk.js
+++ aws-cdk/bin/aws-cdk.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib');
-const { AwsCdkStack } = require('../lib/aws-cdk-stack');
+const { NexScoreStack } = require('../lib/aws-cdk-stack');
 
 const app = new cdk.App();
-new AwsCdkStack(app, 'AwsCdkStack', {
+new NexScoreStack(app, 'NexScoreStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
